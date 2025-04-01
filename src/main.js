c1dcc59f3aaddf379639d63eecab9ac3f2d78371
Arturo Luna <120343+arturoluna@users.noreply.github.com>
2025-03-31 17:22:15 -0700

    example reconfigure

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import './style.css'
 
 document.querySelector('#footer').innerHTML = `
   <div>
-    <p>Current Year: ${new Date().getFullYear()}</p>
+    <p>&copy; MWD: ${new Date().getFullYear()}</p>
   </div>
 `
 
-console.log('Hello Vite!')
+console.log('Hello Vite!!!')
